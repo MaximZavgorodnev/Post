4290f2182dbeaf956d98e30a5f8030ef797ad861
MaximZavgorodnev <maxpek9@yandex.ru>
2021-10-27 12:28:25 +0300

    Добавлены тесты update_failed, update_wasSuccessful, add_notEqualToZero

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,3 +1,4 @@
+
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -7,14 +8,49 @@ class WallServiceTest {
     @Test
     fun add_notEqualToZero() {
         var lastID = 10
-        Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0)
+        var post = Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0)
         val postNew = post.copy(id = lastID)
-        posts += postNew
-        lastID++
-        return posts.last()
+        assertTrue(postNew.id != 0)
     }
 
     @Test
-    fun update() {
+    fun update_wasSuccessful() {
+        // создаём целевой сервис
+        val service = WallService
+
+
+        // заполняем несколькими постами
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 20, text = "PostTwo", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 21, text = "PostThree", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        // создаём информацию об обновлении
+        val update = Post(id = 10, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0)
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertTrue(result)
     }
+
+    @Test
+    fun update_failed() {
+        // создаём целевой сервис
+        val service = WallService
+
+
+        // заполняем несколькими постами
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 20, text = "PostTwo", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        service.add(Post(id = 0, ownerId = 0, fromId = 1, created_by = 0, date = 21, text = "PostThree", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0))
+        // создаём информацию об обновлении
+        val update = Post(id = 20, ownerId = 0, fromId = 1, created_by = 0, date = 11, text = "PostOne", replyOwnerId = 0, replyPostId = 0, friendsOnly = true, comments = null, copyright = null, like = null, repost = null, views = null, postType = "тип", signerId = 0, canPin = true, canDelete = true, canEdit = false, isPinned = true, markedAsAds = false, isFavorite = true, donut = null, postponedId = 0)
+
+        // выполняем целевое действие
+        val result = service.update(update)
+
+        // проверяем результат (используйте assertTrue или assertFalse)
+        assertFalse(result)
+    }
+
 }
